0a0f8cf8cbc5bb6c930d0a5d994cedbb01033074
rnml6 <andayaronmel@gmail.com>
2025-09-20 23:13:16 +0800

    responsive design v5

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -57,7 +57,7 @@ function Content () {
           </div>
 
           <div className='grid grid-cols-[28%_20%_13%_12%_15%_13%] w-full h-auto items_tablehead'>
-            <div className='bg-[#99acff] pl-4 py-3 rounded-tl-[10px] text-xs tracking-widest font-medium name_padding'>
+            <div className='bg-[#99acff] pl-4 py-3 rounded-tl-[10px] text-xs tracking-widest font-medium'>
               NAME
             </div>
             <div className='bg-[#99acff] py-3 text-xs tracking-widest font-medium'>
